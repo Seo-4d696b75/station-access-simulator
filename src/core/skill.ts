79f6230a8350102cdbab368e0a4772ad4548ae73
Seo-4d696b75 <s.kaoru509@gmail.com>
2022-01-20 18:37:55 +0900

    追加：Skill#onActivatedコールバック実装

--- src/core/skill.ts
+++ src/core/skill.ts
@@ -1,6 +1,6 @@
 import * as access from "./access"
 import { Context, fixClock, getCurrentTime } from "./context"
-import { DencoState, getSkill } from "./denco"
+import { copyDencoState, DencoState, getSkill } from "./denco"
 import * as event from "./skillEvent"
 import { SkillPropertyReader } from "./skillManager"
 import { copyUserState, FormationPosition, ReadonlyState, UserState } from "./user"
@@ -282,66 +282,83 @@ export function isSkillActive(skill: SkillPossess): skill is SkillHolder<"posses
  * @returns `active`へ遷移した新しい状態
  */
 export function activateSkill(context: Context, state: ReadonlyState<UserState> & FormationPosition): UserState {
-  const next = copyUserState(state)
+  context = fixClock(context)
+  let next = copyUserState(state)
+  if (!checkActivateSkill(context, state)) {
+    return next
+  }
   const d = next.formation[state.carIndex]
+  if (!d) {
+    context.log.error(`対象のでんこが見つかりません carIndex: ${state.carIndex}, formation.legth: ${state.formation.length}`)
+    throw Error()
+  }
+  const skill = getSkill(d)
+  context.log.log(`スキル状態の変更：${d.name} ${skill.state.type} -> active`)
+  let self = {
+    ...d,
+    carIndex: state.carIndex,
+    skill: skill,
+    skillPropertyReader: skill.propertyReader,
+  }
+  skill.state = {
+    type: "active",
+    data: skill.disactivateAt?.(context, state, self)
+  }
+
+  // callback #onActivated
+  const callback = skill.onActivated
+  if (callback) {
+    // 更新したスキル状態をコピー
+    self = {
+      ...copyDencoState(d),
+      carIndex: state.carIndex,
+      skill: skill,
+      skillPropertyReader: skill.propertyReader,
+    }
+    next = callback(context, next, self)
+  }
+  return refreshSkillState(context, next)
+}
+
+function checkActivateSkill(context: Context, state: ReadonlyState<UserState> & FormationPosition): boolean {
+  const d = state.formation[state.carIndex]
   if (!d) {
     context.log.error(`対象のでんこが見つかりません carIndex: ${state.carIndex}, formation.legth: ${state.formation.length}`)
   }
   const skill = getSkill(d)
-  context = fixClock(context)
   switch (skill.transitionType) {
     case "manual":
     case "manual-condition": {
       switch (skill.state.type) {
         case "idle": {
-          skill.state = {
-            type: "active",
-            data: skill.disactivateAt?.(context, state, {
-              ...d,
-              carIndex: state.carIndex,
-              skill: skill,
-              skillPropertyReader: skill.propertyReader,
-            })
-          }
-          context.log.log(`スキル状態の変更：${d.name} idle -> active`)
-          return refreshSkillState(context, next)
+          return true
         }
         case "active": {
-          return next
+          return false
         }
         default: {
           context.log.error(`スキル状態をactiveに変更できません(state:${skill.state.type},type:${skill.transitionType})`)
-          throw Error()
+          return false
         }
       }
     }
     case "auto": {
       switch (skill.state.type) {
         case "unable": {
-          skill.state = {
-            type: "active",
-            data: skill.disactivateAt?.(context, state, {
-              ...d,
-              carIndex: state.carIndex,
-              skill: skill,
-              skillPropertyReader: skill.propertyReader,
-            })
-          }
-          context.log.log(`スキル状態の変更：${d.name} unable -> active`)
-          return refreshSkillState(context, next)
+          return true
         }
         case "active": {
-          return next
+          return false
         }
         default: {
           context.log.error(`スキル状態をactiveに変更できません(state:${skill.state.type},type:auto)`)
-          throw Error()
+          return false
         }
       }
     }
     default: {
       context.log.error(`スキル状態をactiveに変更できません type:${skill.transitionType}`)
-      throw Error()
+      return false
     }
   }
 }
@@ -432,7 +449,7 @@ function refreshSkillStateOne(context: Context, state: UserState, idx: number): 
   if (denco.skillHolder.type !== "possess") {
     return state
   }
-  const skill = denco.skillHolder.skill
+  const skill = getSkill(denco)
   switch (skill.transitionType) {
     case "always": {
       if (skill.state.type === "not_init") {
@@ -522,7 +539,7 @@ function refreshSkillStateOne(context: Context, state: UserState, idx: number): 
         throw Error()
       }
       let self = {
-        ...denco,
+        ...copyDencoState(denco),
         carIndex: idx,
         skill: skill,
         skillPropertyReader: skill.propertyReader,
@@ -533,6 +550,18 @@ function refreshSkillStateOne(context: Context, state: UserState, idx: number): 
         skill.state = {
           type: "active",
           data: undefined
+        }
+        const callback = skill.onActivated
+        if (callback) {
+          // 更新したスキル状態をコピー
+          self = {
+            ...copyDencoState(denco),
+            carIndex: idx,
+            skill: skill,
+            skillPropertyReader: skill.propertyReader,
+          }
+          state = callback(context, state, self)
+          state = copyUserState(state)
         }
       } else if (!active && skill.state.type === "active") {
         context.log.log(`スキル状態の変更：${denco.name} active -> unable`)
---
+++ src/test/skill.test.ts
@@ -0,0 +1,176 @@
+import { activateSkill, isSkillActive, Skill, SkillActiveTimeout } from "../core/skill"
+import { initContext } from "../core/context"
+import { DencoState, getSkill } from "../core/denco"
+import { initUser } from "../core/user"
+
+describe("スキル処理", () => {
+  test("manual-activateSkill", () => {
+    const context = initContext("test", "test", false)
+    const now = Date.now()
+    context.clock = now
+    // mock callback
+    const timeout: SkillActiveTimeout = {
+      activeTimeout: now + 1000,
+      cooldownTimeout: now + 2000,
+    }
+    const disactivateAt = jest.fn((_, state, self) => timeout)
+    const onActivated = jest.fn((_, state, self) => state)
+    const skill: Skill = {
+      level: 1,
+      name: "test-skill",
+      transitionType: "manual",
+      state: {
+        type: "not_init",
+        data: undefined
+      },
+      propertyReader: jest.fn(),
+      onActivated: onActivated,
+      disactivateAt: disactivateAt,
+    }
+    let denco: DencoState = {
+      level: 5,
+      name: "denco",
+      numbering: "test",
+      currentExp: 0,
+      nextExp: 100,
+      currentHp: 50,
+      maxHp: 50,
+      ap: 10,
+      link: [],
+      film: {},
+      type: "supporter",
+      attr: "flat",
+      skillHolder: {
+        type: "possess",
+        skill: skill
+      }
+    }
+    const state = initUser(context, "test-user", [denco])
+    denco = state.formation[0]
+    expect(getSkill(denco).state.type).toBe("idle")
+    const next = activateSkill(context, { ...state, carIndex: 0 })
+    // state: active変更前
+    expect(disactivateAt.mock.calls.length).toBe(1)
+    // state: active変更前
+    denco = next.formation[0]
+    expect(isSkillActive(denco.skillHolder)).toBe(true)
+    expect(getSkill(denco).state.data).toMatchObject(timeout)
+    expect(onActivated.mock.calls.length).toBe(1)
+    expect(onActivated.mock.calls[0][1]).toMatchObject(next)
+    expect(onActivated.mock.calls[0][2]).toMatchObject(denco)
+  })
+  test("manual-condition-activateSkill", () => {
+    const context = initContext("test", "test", false)
+    const now = Date.now()
+    context.clock = now
+    // mock callback
+    const timeout: SkillActiveTimeout = {
+      activeTimeout: now + 1000,
+      cooldownTimeout: now + 2000,
+    }
+    const canEnabled = jest.fn((_, state, self) => true)
+    const disactivateAt = jest.fn((_, state, self) => timeout)
+    const onActivated = jest.fn((_, state, self) => state)
+    const skill: Skill = {
+      level: 1,
+      name: "test-skill",
+      transitionType: "manual-condition",
+      state: {
+        type: "not_init",
+        data: undefined
+      },
+      propertyReader: jest.fn(),
+      onActivated: onActivated,
+      disactivateAt: disactivateAt,
+    }
+    let denco: DencoState = {
+      level: 5,
+      name: "denco",
+      numbering: "test",
+      currentExp: 0,
+      nextExp: 100,
+      currentHp: 50,
+      maxHp: 50,
+      ap: 10,
+      link: [],
+      film: {},
+      type: "supporter",
+      attr: "flat",
+      skillHolder: {
+        type: "possess",
+        skill: skill
+      }
+    }
+    expect(() => initUser(context, "test-user", [denco])).toThrowError()
+    skill.canEnabled = canEnabled
+    let state = initUser(context, "test-user", [denco])
+    denco = state.formation[0]
+    expect(getSkill(denco).state.type).toBe("idle")
+    expect(canEnabled.mock.calls.length).toBe(1)
+    let next = activateSkill(context, { ...state, carIndex: 0 })
+    // state: active変更前
+    expect(disactivateAt.mock.calls.length).toBe(1)
+    // state: active変更前
+    denco = next.formation[0]
+    expect(isSkillActive(denco.skillHolder)).toBe(true)
+    expect(getSkill(denco).state.data).toMatchObject(timeout)
+    expect(onActivated.mock.calls.length).toBe(1)
+    expect(onActivated.mock.calls[0][1]).toMatchObject(next)
+    expect(onActivated.mock.calls[0][2]).toMatchObject(denco)
+  })
+  test("auto-activateSkill", () => {
+    const context = initContext("test", "test", false)
+    const now = Date.now()
+    context.clock = now
+    // mock callback
+    const timeout: SkillActiveTimeout = {
+      activeTimeout: now + 1000,
+      cooldownTimeout: now + 2000,
+    }
+    const disactivateAt = jest.fn((_, state, self) => timeout)
+    const onActivated = jest.fn((_, state, self) => state)
+    const skill: Skill = {
+      level: 1,
+      name: "test-skill",
+      transitionType: "auto",
+      state: {
+        type: "not_init",
+        data: undefined
+      },
+      propertyReader: jest.fn(),
+      onActivated: onActivated,
+      disactivateAt: disactivateAt,
+    }
+    let denco: DencoState = {
+      level: 5,
+      name: "denco",
+      numbering: "test",
+      currentExp: 0,
+      nextExp: 100,
+      currentHp: 50,
+      maxHp: 50,
+      ap: 10,
+      link: [],
+      film: {},
+      type: "supporter",
+      attr: "flat",
+      skillHolder: {
+        type: "possess",
+        skill: skill
+      }
+    }
+    const state = initUser(context, "test-user", [denco])
+    denco = state.formation[0]
+    expect(getSkill(denco).state.type).toBe("unable")
+    const next = activateSkill(context, { ...state, carIndex: 0 })
+    // state: active変更前
+    expect(disactivateAt.mock.calls.length).toBe(1)
+    // state: active変更前
+    denco = next.formation[0]
+    expect(isSkillActive(denco.skillHolder)).toBe(true)
+    expect(getSkill(denco).state.data).toMatchObject(timeout)
+    expect(onActivated.mock.calls.length).toBe(1)
+    expect(onActivated.mock.calls[0][1]).toMatchObject(next)
+    expect(onActivated.mock.calls[0][2]).toMatchObject(denco)
+  })
+})
