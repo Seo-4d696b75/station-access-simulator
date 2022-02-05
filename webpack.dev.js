bf47166ca84f0b0fa3e54c50d82611a2d292b253
Seo-4d696b75 <s.kaoru509@gmail.com>
2022-02-05 23:49:26 +0900

    修正：デバック用スクリプト

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,12 @@
 const { merge } = require('webpack-merge'); // v5以上は { merge } = require('webpack-merge') とする
 const common = require('./webpack.config.js');
+const path = require("path");
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
 });
