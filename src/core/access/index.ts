0c0fe2271621b5673e6596325a7db0c2366a25ef
k_senda <k_senda@yumemi.co.jp>
2022-10-10 19:31:37 +0900

    修正 access/damage 分離

--- src/core/access/index.ts
+++ src/core/access/index.ts
@@ -1,5 +1,6 @@
 export * from "./access"
 export { counterAttack } from "./counter"
+export * from "./damage"
 export { AccessDencoResult, AccessResult, AccessUserResult } from "./result"
 export { ScoreExpState, ScorePredicate } from "./score"
 export * from "./skill"
