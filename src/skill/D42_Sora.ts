de19c76543ee12c5b91639a90f47645398c19fe6
k_senda <k_senda@yumemi.co.jp>
2022-11-04 17:36:47 +0900

    追加：D42 Sora

---
+++ src/skill/D42_Sora.ts
@@ -0,0 +1,25 @@
+import moment from "moment-timezone";
+import { isWeekendOrHoliday } from "../core/date";
+import { SkillLogic } from "../core/skill";
+
+const skill: SkillLogic = {
+  evaluate: (context, state, step, self) => {
+    if (step === "damage_common" && self.who === "offense") {
+      return {
+        probability: self.skill.property.readNumber("probability"),
+        recipe: (state) => {
+          const atk = self.skill.property.readNumber("ATK")
+          state.attackPercent += atk
+          context.log.log(`私は平日の「お仕事タイム」にたくさん頑張ってます ATK+${atk}%`)
+        }
+      }
+    }
+  },
+  canActivated: (context, state, self) => {
+    if (isWeekendOrHoliday(context.currentTime)) return false
+    const hour = moment(context.currentTime).hour()
+    return 10 <= hour && hour < 17
+  },
+}
+
+export default skill
