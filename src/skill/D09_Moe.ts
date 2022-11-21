41fc6f77902d44efad1fa69116654b37152165c8
k_senda <k_senda@yumemi.co.jp>
2022-11-21 23:52:14 +0900

    修正：D09 Moe

--- src/skill/D09_Moe.ts
+++ src/skill/D09_Moe.ts
@@ -1,5 +1,5 @@
 import { triggerSkillAtEvent } from "../core/event";
-import { isSkillActive, SkillLogic } from "../core/skill";
+import { SkillLogic } from "../core/skill";
 
 const skill: SkillLogic = {
   transitionType: "auto-condition",
@@ -8,19 +8,17 @@ const skill: SkillLogic = {
     return idx >= 0
   },
   onHourCycle: (context, state, self) => {
-    if (isSkillActive(self.skill)) {
-      return triggerSkillAtEvent(context, state, self, (state) => {
-        const heal = self.skill.property.readNumber("heal")
-        context.log.log(`編成内のみなさまのHPを回復いたしますよ♪ +${heal}%`)
-        state.formation.forEach(d => {
-          if (d.currentHp < d.maxHp) {
-            const v = Math.min(Math.floor(d.currentHp + d.maxHp * heal / 100.0), d.maxHp)
-            context.log.log(`HPの回復 ${d.name} ${d.currentHp} > ${v}`)
-            d.currentHp = v
-          }
-        })
+    return triggerSkillAtEvent(context, state, self, (state) => {
+      const heal = self.skill.property.readNumber("heal")
+      context.log.log(`編成内のみなさまのHPを回復いたしますよ♪ +${heal}%`)
+      state.formation.forEach(d => {
+        if (d.currentHp < d.maxHp) {
+          const v = Math.min(Math.floor(d.currentHp + d.maxHp * heal / 100.0), d.maxHp)
+          context.log.log(`HPの回復 ${d.name} ${d.currentHp} > ${v}`)
+          d.currentHp = v
+        }
       })
-    }
+    })
   }
 }
 
