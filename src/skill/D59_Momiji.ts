422b2dd2c69e527a923b77d5b95a0109caa8242f
k_senda <k_senda@yumemi.co.jp>
2022-11-23 21:08:10 +0900

    追加：D59 Momiji

---
+++ src/skill/D59_Momiji.ts
@@ -0,0 +1,24 @@
+import { getDefense } from "../core/access";
+import { countDencoType } from "../core/denco";
+import { formatPercent } from "../core/format";
+import { SkillLogic } from "../core/skill";
+
+const skill: SkillLogic = {
+  transitionType: "always",
+  triggerOnAccess: (context, state, step, self) => {
+    if (step === "damage_common" && self.who === "offense") {
+      return {
+        probability: self.skill.property.readNumber("probability"),
+        recipe: (state) => {
+          const cnt = countDencoType(getDefense(state).formation)
+          const unit = self.skill.property.readNumber("ATK")
+          const atk = unit * cnt
+          state.attackPercent += atk
+          context.log.log(`いろんなでんこに会えると、嬉しくなって勢いあまっちゃうんだよ～ ATK${formatPercent(atk)}`)
+        }
+      }
+    }
+  }
+}
+
+export default skill
