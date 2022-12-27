4e43af2cc5d761440c90af81471729c37a7c8860
k_senda <k_senda@yumemi.co.jp>
2022-12-28 01:17:16 +0900

    修正：２回以上のカウンターが発動しない不具合
    `after_damage`のスキル処理を全体の最後で１回しか処理しなと、
    まりか > みこと・くにの場合でまりかのカウンターが発動しない

--- src/core/access/start.ts
+++ src/core/access/start.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs"
-import { AccessDencoState, AccessResult, AccessSide, AccessSideState, AccessState, getAccessDenco, getDefense, getFormation, getSide, hasDefense } from "."
+import { AccessDencoState, AccessResult, AccessSide, AccessSideState, AccessState, getAccessDenco, getDefense, hasDefense } from "."
 import { copy } from "../../"
 import { Context, withFixedClock } from "../context"
 import { TIME_FORMAT } from "../date"
@@ -8,12 +8,12 @@ import { ReadonlyState } from "../state"
 import { Station } from "../station"
 import { UserState } from "../user"
 import { getUserPropertyReader } from "../user/property"
+import { triggerSkillAfterDamage } from "./afterDamage"
 import { runAccessDamageCalculation } from "./damage"
-import { completeDencoHP, updateDencoHP } from "./hp"
+import { completeDencoHP } from "./hp"
 import { completeAccess } from "./result"
 import { calcAccessBonusScoreExp, calcLinkBonusScoreExp } from "./score"
-import { checkProbabilityBoost, filterValidatedSkill, hasValidatedSkill, triggerSkillAt, triggerSkillOnSide } from "./skill"
-
+import { checkProbabilityBoost, hasValidatedSkill, triggerSkillAt } from "./skill"
 /**
  * アクセス処理の入力・設定を定義します
  */
@@ -97,14 +97,12 @@ export const startAccess = (context: Context, config: AccessConfig): AccessResul
     // ピンク
     state.linkDisconnected = true
     state.linkSuccess = true
+    state = triggerSkillAfterDamage(context, state)
   } else {
     // 相手不在
     state.linkSuccess = true
+    state = triggerSkillAfterDamage(context, state)
   }
-
-
-
-  state = triggerSkillAfterDamage(context, state)
 
   decideLinkResult(context, state)
 
@@ -177,117 +175,6 @@ function initAccessDencoState(context: Context, f: ReadonlyState<UserState>, car
       link: 0
     },
   }
-}
-
-interface AfterDamageSkillEntry {
-  carIndex: number,
-  hp: number
-}
-
-function _triggerSkillAfterDamage(
-  context: Context,
-  state: AccessState,
-  offenseQueue: AfterDamageSkillEntry[] | undefined,
-  defenseQueue: AfterDamageSkillEntry[] | undefined,
-  which: AccessSide,
-): AccessState {
-  // 終了条件
-  // 攻撃・守備側ともに検査対象が0件
-  if (offenseQueue?.length === 0 && defenseQueue?.length === 0) {
-    return state
-  }
-  // 検査対象の選択
-  let queue = which === "offense" ? offenseQueue : (
-    // 相手不在の場合は検査対象は0件
-    state.defense ? defenseQueue : []
-  )
-  // 検査対象の更新
-  if (!queue) {
-    const formation = getFormation(state, which)
-    // 初期化（編成内全員を確認）
-    queue = filterValidatedSkill(state, which).map(idx => ({
-      carIndex: idx,
-      hp: formation[idx].hpAfter,
-    }))
-  } else if (queue.length > 0) {
-    const side = getSide(state, which)
-    // 前回の発動判定時と比べて まだ発動していない＆HPの変化があった
-    queue = queue.filter(e => {
-      let self = side.formation[e.carIndex]
-      let hasTriggered = side.triggeredSkills.some(t => t.numbering === self.numbering && t.step === "after_damage")
-      let hpChanged = (e.hp !== self.hpAfter)
-      return !hasTriggered && hpChanged
-    }).map(e => ({
-      ...e,
-      hp: side.formation[e.carIndex].hpAfter
-    }))
-    if (queue.length > 0) {
-      context.log.log("スキルの評価中にHPが変化したでんこがいます")
-      queue = queue.map(e => {
-        let self = side.formation[e.carIndex]
-        context.log.log(`  denco:${self.name} HP:${e.hp} => ${self.hpAfter}`)
-        return {
-          carIndex: e.carIndex,
-          hp: self.hpAfter
-        }
-      })
-      context.log.log("スキルを再度評価：ダメージ計算完了後")
-    }
-  }
-  // 検査
-  if (queue.length > 0) {
-    // スキル発動（必要なら）
-    state = triggerSkillOnSide(
-      context,
-      state,
-      "after_damage",
-      which,
-      queue.map(e => e.carIndex),
-    )
-    // HPの決定 & HP0 になったらリブート
-    // 全員確認する
-    updateDencoHP(context, state, "offense")
-    updateDencoHP(context, state, "defense")
-  }
-  // 次の検査
-  if (which === "offense") {
-    return _triggerSkillAfterDamage(
-      context,
-      state,
-      queue,
-      defenseQueue,
-      "defense"
-    )
-  } else {
-    return _triggerSkillAfterDamage(
-      context,
-      state,
-      offenseQueue,
-      queue,
-      "offense"
-    )
-  }
-}
-
-function triggerSkillAfterDamage(context: Context, state: AccessState): AccessState {
-
-  context.log.log("アクセス結果を仮決定")
-  context.log.log(`攻撃側のリンク成果：${state.linkSuccess}`)
-  if (state.defense) {
-    context.log.log(`守備側のリンク解除：${state.linkDisconnected}`)
-  } else {
-    context.log.log(`守備側のリンク解除：不在`)
-  }
-
-  context.log.log("スキルを評価：ダメージ計算完了後")
-
-  return _triggerSkillAfterDamage(
-    context,
-    state,
-    undefined,
-    undefined,
-    "offense"
-  )
 }
 
 function logAccessStart(context: Context, state: ReadonlyState<AccessState>) {
