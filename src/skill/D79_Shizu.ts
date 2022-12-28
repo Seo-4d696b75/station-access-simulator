e9a117f9883d0d1337ea8f35800f02dc28d6b60c
k_senda <k_senda@yumemi.co.jp>
2022-12-28 15:16:26 +0900

    追加：D79 Shizu

---
+++ src/skill/D79_Shizu.ts
@@ -0,0 +1,36 @@
+import { formatPercent } from "../core/format";
+import { SkillLogic } from "../core/skill";
+
+const skill: SkillLogic = {
+  transitionType: "manual",
+  deactivate: "default_timeout",
+  triggerOnAccess: (context, state, step, self) => {
+    if (step === "damage_common" && self.which === "offense") {
+      const heatCnt = state
+        .offense
+        .formation
+        .filter(d => d.attr === "heat")
+        .length
+      if (heatCnt === state.offense.formation.length) {
+        return {
+          probabilityKey: "probability",
+          recipe: (state) => {
+            const atk = self.skill.property.readNumber("ATK")
+            state.attackPercent += atk
+            const defUnit = self.skill.property.readNumber("DEF")
+            const attrCnt = new Set(
+              state.defense!.formation.map(d => d.attr)
+            ).size
+            const def = defUnit * attrCnt
+            state.defendPercent += def
+            context.log.log(`真のテンサイはサポートもうまくなくちゃね。`)
+            context.log.log(`  ATK ${formatPercent(atk)}`)
+            context.log.log(`  DEF ${formatPercent(def)}(${defUnit} x ${attrCnt})`)
+          }
+        }
+      }
+    }
+  }
+}
+
+export default skill
