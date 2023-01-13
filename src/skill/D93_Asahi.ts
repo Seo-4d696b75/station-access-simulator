b05dcaa38b32ee2a11b035a5045cc1d10b3facbd
k_senda <k_senda@yumemi.co.jp>
2023-01-13 23:42:18 +0900

    追加：D93 Asahi

---
+++ src/skill/D93_Asahi.ts
@@ -0,0 +1,27 @@
+import { formatPercent } from "../core/format";
+import { isSkillActive, SkillLogic } from "../core/skill";
+
+const skill: SkillLogic = {
+  transitionType: "manual",
+  deactivate: "default_timeout",
+  triggerOnAccess: (context, state, step, self) => {
+    if (step === "damage_common" && self.who === "defense") {
+      return {
+        probabilityKey: "probability", // 100%
+        recipe: (state) => {
+          let def = self.skill.property.readNumber("DEF")
+          const activeCnt = state.offense.formation
+            .filter(d => isSkillActive(d.skill))
+            .length
+          if (activeCnt >= 4) {
+            def += self.skill.property.readNumber("DEF_additional")
+          }
+          state.defendPercent += def
+          context.log.log(`ヘコんでないで楽しくいこー！DEF${formatPercent(def)}`)
+        }
+      }
+    }
+  }
+}
+
+export default skill
