956956b29f38c87b007cd5c090060ce4cd938354
k_senda <k_senda@yumemi.co.jp>
2023-01-30 23:52:32 +0900

    修正：skill  ~25

--- src/skill/D22_Ren.ts
+++ src/skill/D22_Ren.ts
@@ -1,27 +1,20 @@
-import { getAccessDenco } from "../core/access/index";
 import { SkillLogic } from "../core/skill";
 
 
 const skill: SkillLogic = {
   transitionType: "manual",
   deactivate: "default_timeout",
-  triggerOnAccess: (context, state, step, self) => {
-    if (step === "before_access" && self.who === "offense" && state.defense && !state.pinkMode) {
-      const defense = getAccessDenco(state, "defense")
+  onAccessBeforeStart: (context, state, self) => {
+    if (self.who === "offense" && state.defense && !state.pinkMode) {
       // TODO 無効化対象のでんこを列挙する 適宜変更が必要
       const target = self.skill.property.readStringArray("invalidated")
-      if (target.includes(defense.numbering)) {
-        return {
-          probability: "probability",
-          recipe: (state) => {
-            const defense = getAccessDenco(state, "defense")
-            defense.skillInvalidated = true
-            context.log.log(`ウチのスキルは相手のスキルを無効化するでぇー target:${defense.name}`)
-          }
-        }
+      return {
+        probability: self.skill.property.readNumber("probability", 100),
+        type: "invalidate_skill",
+        isTarget: (d) => d.who === "defense" && target.includes(d.numbering)
       }
     }
-  },
+  }
 }
 
 export default skill
