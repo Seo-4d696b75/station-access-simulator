bec5ff7d2422a5a97136070965ec2f2583a23d45
k_senda <k_senda@yumemi.co.jp>
2023-02-19 18:47:25 +0900

    修正：D40 Haru

--- src/skill/D40_Haru.ts
+++ src/skill/D40_Haru.ts
@@ -1,5 +1,4 @@
-import { getDefense } from "../core/access/index";
-import { isSkillActive, SkillLogic } from "../core/skill";
+import { SkillLogic } from "../core/skill";
 
 const skill: SkillLogic = {
   transitionType: "always",
@@ -13,32 +12,6 @@ const skill: SkillLogic = {
         probability: self.skill.property.readNumber("probability"),
         type: "invalidate_skill",
         isTarget: (d) => d.type === "supporter"
-      }
-    }
-  },
-  triggerOnAccess: (context, state, step, self) => {
-    if (step === "before_access" && self.who === "offense" && state.defense && !state.pinkMode) {
-      const all = [
-        ...state.offense.formation,
-        ...getDefense(state).formation
-      ]
-      const anySupporter = all.some(d => {
-        return d.type === "supporter" && isSkillActive(d.skill) && !d.skillInvalidated
-      })
-      if (anySupporter) {
-        return {
-          probability: "probability",
-          recipe: (state) => {
-            const all = [
-              ...state.offense.formation,
-              ...getDefense(state).formation
-            ]
-            const target = all.filter(d => d.type === "supporter" && isSkillActive(d.skill))
-            const names = target.map(d => d.name).join(",")
-            target.forEach(d => d.skillInvalidated = true)
-            context.log.log(`ハルはひとりで大丈夫だもん！！ 無効化：${names}`)
-          }
-        }
       }
     }
   }
